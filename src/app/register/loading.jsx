b18f2aa77b923a124dc389d3a2b1fe499d0dca74
RidloGhifary <ridloghfry@gmail.com>
2024-06-12 08:43:46 +0700

    feat: complete creating register page

---
+++ src/app/register/loading.jsx
@@ -0,0 +1,13 @@
+export default function Loading() {
+  return (
+    <div className="space-y-3 py-16 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="bg-gray-400 w-[50%] h-20 rounded-md animate-pulse" />
+      <div className="bg-gray-400 w-full h-20 rounded-md animate-pulse" />
+      <div className="bg-gray-400 w-[50%] h-20 rounded-md animate-pulse" />
+      <div className="bg-gray-400 w-full h-20 rounded-md animate-pulse" />
+      <div className="bg-gray-400 w-[50%] h-20 rounded-md animate-pulse" />
+      <div className="bg-gray-400 w-full h-20 rounded-md animate-pulse" />
+      <div className="bg-gray-400 w-[70%] h-20 rounded-md animate-pulse" />
+    </div>
+  );
+}

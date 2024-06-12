9fca020aa80af8a95edb6440d293a4fcdaea7f53
RidloGhifary <ridloghfry@gmail.com>
2024-06-12 12:50:48 +0700

    style: navbar image size changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,34 +15,37 @@ export default function Navbar() {
 
   return (
     <header className="flex w-full items-center justify-between p-4 lg:px-6">
-      <Link href="/" className="flex flex-1 items-center gap-1">
+      <Link href="/" className="flex flex-1 items-center">
         <Image
           src="/logo.webp"
           alt="logo-company"
           loading="lazy"
           width={50}
           height={50}
-          className="aspect-square h-14 w-1h-14 object-contain"
+          className="aspect-square object-contain"
         />
       </Link>
 
-      <nav className="hidden flex-1 items-center justify-center gap-8 md:flex">
+      <nav className="hidden flex-1 items-center justify-center gap-5 md:flex">
         <Link href="/" className="font-semibold text-mainColor hover:underline">
           Home
         </Link>
         <Link
           href="/about"
-          className="font-semibold text-mainColor hover:underline">
+          className="font-semibold text-mainColor hover:underline"
+        >
           About
         </Link>
         <Link
           href="/cars"
-          className="font-semibold text-mainColor hover:underline">
+          className="font-semibold text-mainColor hover:underline"
+        >
           Cars
         </Link>
         <Link
           href="/teams"
-          className="font-semibold text-mainColor hover:underline">
+          className="font-semibold text-mainColor hover:underline"
+        >
           Teams
         </Link>
       </nav>
@@ -50,7 +53,8 @@ export default function Navbar() {
       <div className="mr-4 flex flex-1 items-center justify-end gap-4 md:mr-0 md:gap-8">
         <Link
           href="/login"
-          className="flex items-center justify-center gap-4 border border-mainColor bg-mainColor px-4 py-1 font-semibold text-white transition hover:bg-white hover:text-mainColor md:px-6 md:py-2">
+          className="flex items-center justify-center gap-4 border border-mainColor bg-mainColor px-4 py-1 font-semibold text-white transition hover:bg-white hover:text-mainColor md:px-6 md:py-2"
+        >
           Login
         </Link>
       </div>
@@ -76,7 +80,8 @@ export default function Navbar() {
       <section
         className={`border-orange fixed ${
           showNavbarMobile ? "left-0" : "left-[-200%]"
-        } top-0 z-50 h-[100vh] w-[60%] border-r bg-white p-4 shadow-md transition-all duration-300 ease-linear`}>
+        } top-0 z-50 h-[100vh] w-[60%] border-r bg-white p-4 shadow-md transition-all duration-300 ease-linear`}
+      >
         <Link href="/" className="flex flex-1 items-center gap-1">
           <Image
             src="/logo.webp"
@@ -84,7 +89,7 @@ export default function Navbar() {
             loading="lazy"
             width={50}
             height={50}
-            className="aspect-square h-14 w-1h-14 object-contain"
+            className="w-1h-14 aspect-square h-14 object-contain"
           />
         </Link>
 
@@ -92,22 +97,26 @@ export default function Navbar() {
           <div className="flex flex-col gap-8">
             <Link
               href="/"
-              className="font-semibold text-mainColor hover:underline">
+              className="font-semibold text-mainColor hover:underline"
+            >
               Home
             </Link>
             <Link
               href="/about"
-              className="font-semibold text-mainColor hover:underline">
+              className="font-semibold text-mainColor hover:underline"
+            >
               About
             </Link>
             <Link
               href="/cars"
-              className="font-semibold text-mainColor hover:underline">
+              className="font-semibold text-mainColor hover:underline"
+            >
               Cars
             </Link>
             <Link
               href="/teams"
-              className="font-semibold text-mainColor hover:underline">
+              className="font-semibold text-mainColor hover:underline"
+            >
               Teams
             </Link>
           </div>
